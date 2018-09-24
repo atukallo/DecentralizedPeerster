5811886166d5194a03c1fd7668ee030734260817
atukallo <alex.tukallo@gmail.com>
2018-09-24 22:41:19 +0200

    project - initial structure added

---
+++ src/github.com/SubutaiBogatur/Peerster/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+// command line arguments
+var (
+	UIPort     = flag.Int("UIPort", 4848, "Port, where client is launched. Client is launched on 127.0.0.1:{port}")
+	gossipAddr = flag.String("gossipAddr", "127.0.0.1:1212", "Address, where gossiper is launched")
+	name       = flag.String("name", "go_rbachev", "Gossiper name")
+	peers      = flag.String("peers", "", "Other gossipers' addresses separated with \",\"")
+	simple     = flag.Bool("simple", true, "True, if mode is simple??")
+)
+
+func main() {
+	flag.Parse()
+	fmt.Println(*UIPort)
+}
